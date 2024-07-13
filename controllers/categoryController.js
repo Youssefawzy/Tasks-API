b8adb0f3811b9f6791bafd9e4791a737d72db119
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-07-13 18:23:12 +0300

    handling errors in category controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,45 +1,66 @@
 const Category = require("./../models/categoryModel");
+const Task = require("./../models/taskModel");
 
 exports.createCategory = async (req, res, next) => {
-  const { name } = req.body;
-  const user = req.user;
-  console.log({ name, user });
-  const category = await Category.create({ name, user });
+  try {
+    const { name } = req.body;
+    const user = req.user;
+    console.log({ name, user });
+    const category = await Category.create({ name, user });
 
-  res.status(200).json({
-    status: "success",
-    Category: category,
-  });
+    res.status(200).json({
+      status: "success",
+      Category: category,
+    });
+  } catch (error) {
+    res.json({
+      error: error.message,
+      error: error.stack,
+    });
+  }
 };
 
 exports.getAllCategories = async (req, res) => {
-  const categories = await Category.find().populate("user");
+  try {
+    const categories = await Category.find().populate("user");
 
-  // SEND RESPONSE
-  res.status(200).json({
-    status: "success",
-    results: categories.length,
-    Categories: {
-      Categories,
-    },
-  });
+    res.status(200).json({
+      status: "success",
+      results: categories.length,
+      Categories: {
+        categories,
+      },
+    });
+  } catch (error) {
+    res.json({
+      error: error.message,
+      error: error.stack,
+    });
+  }
 };
 
 exports.getCategory = async (req, res) => {
-  const category = await Category.findById(req.params.id).populate("user");
+  try {
+    const category = await Category.findById(req.params.id).populate("user");
 
-  if (!category) {
-    const error = new Error("No category found with that ID");
-    error.status = 404;
-    throw error;
+    if (!category) {
+      const error = new Error("No category found with that ID");
+      error.status = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      status: "success",
+      Category: {
+        category,
+      },
+    });
+  } catch (error) {
+    res.json({
+      error: error.message,
+      error: error.stack,
+    });
   }
-
-  res.status(200).json({
-    status: "success",
-    Category: {
-      category,
-    },
-  });
 };
 
 exports.deleteCategory = async (req, res) => {
