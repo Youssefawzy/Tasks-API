d403469109ec31963501e36f4436a2b636b44c3f
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-07-14 17:45:20 +0300

    change the category type to string

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const Path = require("path");
 const morgan = require("morgan");
 const userRouter = require("./routes/userRoute");
 const categoryRouter = require("./routes/categoryRoute");
+const taskRouter = require("./routes/taskRoute");
 
 const app = express();
-app.use(morgan())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tasks", taskRouter);
 app.use("/api/v1/categories", categoryRouter);
 
 module.exports = app;
